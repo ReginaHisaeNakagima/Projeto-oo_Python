196a3528e11e9e132c0079fd7056cb172fa29444
HisaeNakagima <1823289@aluno.univesp.br>
2023-08-24 18:37:14 -0300

    Usando Getters e Setters

--- conta.py
+++ conta.py
@@ -5,12 +5,28 @@ class Conta:
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
+
     def extrato(self):
         print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
+
     def deposita(self, valor):
         self.__saldo += valor
+
     def saca(self, valor):
         self.__saldo -= valor
+
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    def get_limite(self):
+        return self.__limite
+
+    def set_limite(self, limite):
+        self.__limite = limite
