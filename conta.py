b99ec25f475fa5ad8009a13bc3739b2db7319642
HisaeNakagima <1823289@aluno.univesp.br>
2023-08-25 15:38:30 -0300

    Métodos privados

--- conta.py
+++ conta.py
@@ -12,15 +12,22 @@ class Conta:
     def deposita(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
     def saca(self, valor):
-        self.__saldo -= valor
+        if (self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
 
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
 
-    def get_saldo(self):
-        return self.__saldo
+    #def get_saldo(self):
+        #return self.__saldo
 
     def get_titular(self):
         return self.__titular
@@ -32,6 +39,10 @@ class Conta:
         #self.__limite = limite
 
     # Utilizando propriedade para o limite
+    @property
+    def saldo(self):
+        return self.__saldo
+
     @property
     def limite(self):
         return self.__limite
